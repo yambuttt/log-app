33a3fe3743193fe267132ae7a08f50f0a82541e7
yambuttt <hanyagabuttt0@gmail.com>
2026-06-30 03:57:58 +0700

    Test Remote github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        <!-- Styles / Scripts -->
+        <!-- Styles / Scripts Test Remote github-->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
